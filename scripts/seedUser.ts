8210b6f89bc142741e74e133eb7a68d01c48f791
desarrollador-hseq <desarrolladorhseq@hotmail.com>
2023-12-18 14:43:02 -0500

    actualizando seed user, threshold y cities

--- scripts/seedUser.ts
+++ scripts/seedUser.ts
@@ -1,14 +1,77 @@
 
 
+const { PrismaClient } = require("@prisma/client")
+const database = new PrismaClient()
 
 async function main() {
-    const { PrismaClient } = require("@prisma/client")
-    const database = new PrismaClient()
     try {
         await database.user.createMany({
             data: [
-                {username: "administrator", password: "$2a$10$ZC9hFJAv/pQL9qFHtOF2nOzt/t8UA0BhcGfCkd9sgPf4HFmjxzmeG", role: "ADMIN"},
-                {username: "viewer", password: "$2a$10$ZC9hFJAv/pQL9qFHtOF2nOzt/t8UA0BhcGfCkd9sgPf4HFmjxzmeG", role: "VIEWER"},
+                {username: "administrator", password: "$2a$10$lA5JIqCs5R8Eae2FgYMQTeEc1VXx2MTsQC9ql0xNIxnBlXXexN4GS", role: "ADMIN"},
+                {username: "viewer", password: "$2a$10$obvjJMBoXAme55FJVAqrTuTJySFurGrrgXefvf7rYPtfpSbe5YJsa", role: "VIEWER"},
+            ]
+        })
+
+        console.log("Success")
+    } catch (error) {
+        console.log("Error seeding the database categories", error)
+    } finally {
+        await database.$disconnect()
+    }
+}
+
+async function mainwd() {
+    try {
+        await database.formationParameters.create({
+            data: {
+                threshold: 80
+            }
+        })
+
+        console.log("Success")
+    } catch (error) {
+        console.log("Error seeding the database categories", error)
+    } finally {
+        await database.$disconnect()
+    }
+}
+
+async function mainw() {
+    try {
+        await database.city.createMany({
+            data: [
+
+                { realName: "Bogotá", formated: "bogota", color: "#ff5733" },
+                { realName: "Medellín", formated: "medellin", color: "#33ff57" },
+                { realName: "Cali", formated: "cali", color: "#5733ff" },
+                { realName: "Barranquilla", formated: "barranquilla", color: "#ff5733" },
+                { realName: "Cartagena", formated: "cartagena", color: "#33ff57" },
+                { realName: "Cúcuta", formated: "cucuta", color: "#5733ff" },
+                { realName: "Bucaramanga", formated: "bucaramanga", color: "#ff5733" },
+                { realName: "Villavicencio", formated: "villavicencio", color: "#33ff57" },
+                { realName: "Santa Marta", formated: "santa-marta", color: "#5733ff" },
+                { realName: "Ibagué", formated: "ibague", color: "#ff5733" },
+                { realName: "Bello", formated: "bello", color: "#33ff57" },
+                { realName: "Pasto", formated: "pasto", color: "#5733ff" },
+                { realName: "Manizales", formated: "manizales", color: "#ff5733" },
+                { realName: "Neiva", formated: "neiva", color: "#33ff57" },
+                { realName: "Soledad", formated: "soledad", color: "#5733ff" },
+                { realName: "Pereira", formated: "pereira", color: "#ff5733" },
+                { realName: "Montería", formated: "monteria", color: "#33ff57" },
+                { realName: "Itagüí", formated: "itagui", color: "#5733ff" },
+                { realName: "Palmira", formated: "palmira", color: "#ff5733" },
+                { realName: "Buenaventura", formated: "buenaventura", color: "#33ff57" },
+                { realName: "Villamaria", formated: "villamaria", color: "#5733ff" },
+                { realName: "Floridablanca", formated: "floridablanca", color: "#ff5733" },
+                { realName: "Valledupar", formated: "valledupar", color: "#33ff57" },
+                { realName: "Soacha", formated: "soacha", color: "#5733ff" },
+                { realName: "Quibdó", formated: "quibdo", color: "#ff5733" },
+                { realName: "Envigado", formated: "envigado", color: "#33ff57" },
+                { realName: "Florencia", formated: "florencia", color: "#5733ff" },
+                { realName: "Tuluá", formated: "tulua", color: "#ff5733" },
+                { realName: "Tunja", formated: "tunja", color: "#33ff57" },
+                { realName: "Turbo", formated: "turbo", color: "#5733ff" },
+                { realName: "Girardot", formated: "girardot", color: "#ff5733" }
             ]
         })
 
@@ -22,3 +85,7 @@ async function main() {
 
 
 main()
+
+mainwd()
+
+mainw()
